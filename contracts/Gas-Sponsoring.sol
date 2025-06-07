ceb4783bd3f071621337a9e15aef511b8705ef8c
ireneseifert <ireneseifert341@gmail.com>
2025-06-07 14:24:47 +0200

    Update Gas-Sponsoring.sol
    add code

--- contracts/Gas-Sponsoring.sol
+++ contracts/Gas-Sponsoring.sol
@@ -1,1 +1,19 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract GasSponsor {
+    address public relayer;
+    mapping(bytes32 => bool) public executed;
+
+    constructor(address _relayer) { relayer = _relayer; }
+
+    function relay(address to, bytes calldata data, bytes calldata sig) external {
+        require(msg.sender == relayer, "Not relayer");
+        bytes32 hash = keccak256(abi.encodePacked(to, data));
+        require(!executed[hash], "Already done");
+        executed[hash] = true;
+        (bool ok, ) = to.call(data);
+        require(ok, "Call failed");
+    }
+}
